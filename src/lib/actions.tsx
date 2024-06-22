9ca8717f56264613ccbe253d6eb1c2d6f13359b5
aaakash06 <akashbagale182@gmail.com>
2024-06-22 22:15:07 +0545

    db primsa removed

--- src/lib/actions.tsx
+++ src/lib/actions.tsx
@@ -3,10 +3,10 @@
 import { cookies } from "next/headers";
 import bcrypt from "bcrypt";
 const saltRounds = 10;
-import { PrismaClient } from "@prisma/client";
+// import { PrismaClient } from "@prisma/client";
 import { redirect } from "next/navigation";
 import * as jwt from "jsonwebtoken";
-const prisma = new PrismaClient();
+// const prisma = new PrismaClient();
 const tokenSecret = "password";
 
 export async function register(formData: {
@@ -14,42 +14,42 @@ export async function register(formData: {
   password: string;
   name: string;
 }) {
-  await prisma.$connect();
+  // await prisma.$connect();
   const hashedPassword = bcrypt.hashSync(formData.password, saltRounds);
 
-  const createdUser = await prisma.user.create({
-    data: {
-      name: formData.name,
-      email: formData.email,
-      password: hashedPassword!,
-    },
-  });
+  // const createdUser = await prisma.user.create({
+  //   data: {
+  //     name: formData.name,
+  //     email: formData.email,
+  //     password: hashedPassword!,
+  //   },
+  // });
 
   redirect("./");
 }
 export async function login(formData: { email: string; password: string }) {
-  await prisma.$connect();
-  const user = await prisma.user.findFirst({
-    where: {
-      email: formData.email,
-    },
-  });
-  if (!user) {
-    console.log("no user found");
-    return;
-  }
-  const hashedPassword = user.password;
-  const result = await bcrypt.compare(formData.password, hashedPassword);
-  if (result) {
-    const payload = { email: formData.email };
-    const token = jwt.sign(payload, tokenSecret, { expiresIn: 60 * 60 });
-    cookies().set({
-      name: "jwt",
-      value: token,
-      path: "/",
-    });
-    redirect("./");
-  }
-  console.log("login failed");
+  // await prisma.$connect();
+  // const user = await prisma.user.findFirst({
+  //   where: {
+  //     email: formData.email,
+  //   },
+  // });
+  // if (!user) {
+  //   console.log("no user found");
+  //   return;
+  // }
+  // const hashedPassword = user.password;
+  // const result = await bcrypt.compare(formData.password, hashedPassword);
+  // if (result) {
+  //   const payload = { email: formData.email };
+  //   const token = jwt.sign(payload, tokenSecret, { expiresIn: 60 * 60 });
+  //   cookies().set({
+  //     name: "jwt",
+  //     value: token,
+  //     path: "/",
+  //   });
+  //   redirect("./");
+  // }
+  // console.log("login failed");
   return;
 }
